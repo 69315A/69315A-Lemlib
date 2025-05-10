ee2e6dd9de5e6ab3250a2f1f9999c17da8649203
69315A <172042794+69315A@users.noreply.github.com>
2025-05-10 13:47:48 -0500

    main.cpp
    5/10/2025 Post-worlds

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,12 @@
 #include "pros/motors.h"
 
 // Chassis constructors
-pros::MotorGroup leftMotors{{1, 2, -3}, pros::MotorGearset::blue};
+pros::MotorGroup leftMotors{{-1, 2, 3}, pros::MotorGearset::blue};
 pros::MotorGroup rightMotors{{-13, -12, 11}, pros::MotorGearset::blue};
 
 // Motor constructors
 pros::Motor intake(14, pros::MotorGearset::blue);
-pros::MotorGroup lb({-20, 19}, pros::MotorGearset::green);
+pros::MotorGroup lb({19, -20}, pros::MotorGearset::green);
 
 // Pneumatic constructors
 pros::adi::Pneumatics mogoL('A', true);
@@ -24,7 +24,7 @@ pros::adi::Pneumatics doinkerR('D', false);
 
 // Sensor constructors
 pros::Imu imu(17);
-pros::Rotation rSensor(18);
+pros::Rotation rSensor(16);
 pros::Optical oSensor(5);
 
 // Controller constructor
@@ -47,30 +47,81 @@ void retractMogo(){
 
 // ----------- L A D Y B R O W N ----------- //
 // Ladybrown states arrays
-int autonStates[4] = {150, 12000, 25000, 50000};      // Remember: centidegrees; 150, 12000, 50000
-int driverStates[3] = {150, 11000, 50000};
+// int autonStates[4] = {150, 12000, 25000, 50000};      // Remember: centidegrees; 150, 12000, 50000
+int driverStates[3] = {0, 110, 600}; // 600
+int numStates = 3;
+// 1 less 0
 
 // Ladybrown variables
 int currState = 0;
 int target = driverStates[currState];  // Default to driver states
 
 // Ladybrown flags
-bool isAutonMode = false;
+// bool isAutonMode = false;
 bool lbTaskEnabled = true;
 
 // Depending on flag, cycles through states
+// void nextState() {
+//     int numStates = isAutonMode ? 4 : 3;
+//     currState = (currState + 1) % numStates;
+//     target = isAutonMode ? autonStates[currState] : driverStates[currState];
+
+//     if(currState == 0){
+//         brain.printf("\nState rest");
+//     }
+// }
+
+// void nextState(){
+//     currState += 1;
+//     if(currState == 3){
+//         currState = 0;
+//     }
+// }
+
 void nextState() {
-    int numStates = isAutonMode ? 4 : 3;
-    currState = (currState + 1) % numStates;
-    target = isAutonMode ? autonStates[currState] : driverStates[currState];
+
+    currState += 1;
+    if(currState == numStates){
+        currState = 0;
+    }
+    target = driverStates[currState];
+
+    // int numStates = 3;
+    // currState = (currState + 1) % numStates;
+    // target = isAutonMode ? autonStates[currState] : driverStates[currState];
+    // brain.printf("\nState: %d, Target: %d", currState, target);
+	// brain.printf("\nCurrent: %dmA", lb.get_current_draw());
 }
 
 // Set up Ladybrown PID & controls velocity 
 void liftControl() {
-    double kp = 0.5;
-    lb.move(kp * (target - rSensor.get_position())/100.0);
+    double kp = 0.4;
+    lb.move(kp * (target - rSensor.get_position()/100.0));
     lb.set_brake_mode((pros::E_MOTOR_BRAKE_HOLD));
 }
+
+// void debugEncoder() {
+//     while(true) {
+//         brain.printf("Pose: %d", chassis.getPose());
+//         pros::delay(500);
+//     }
+// }
+
+// void liftControl() {
+//     double kp = 0.8;  // Increased gain
+//     int error = target - rSensor.get_position();
+//     int output = -(kp * error);  // Keep negative sign
+    
+//     // Add speed limiting
+//     if (output > 127) output = 127;
+//     if (output < -127) output = -127;
+    
+//     lb.move(output);
+//     lb.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+    
+//     // Debug output
+//     brain.printf("\nTgt:%d Pos:%d Out:%d", target, rSensor.get_position(), output);
+// }
 
 // ----------- C O L O R S O R T -----------//
 // Color Sort Task Variables
@@ -101,42 +152,50 @@ void colorSortTask(void* param) {
 }
 
 rd::Selector selector({
+    {"Blue Positive WQ", NEW_B_P_qual},
+    {"Blue Negative WQ", NEW_B_N_qual},
+    {"Red Positive WQ", NEW_R_P_qual},
+    {"Red Negative WQ", NEW_R_N_qual},
+    {"Testing/tuning", turnTest},
+    {"Blue Positive GOAL RUSH", B_P_goalrush},
     {"Skills", skills},
     {"Red Positive Ring Rush", R_P_ringrush},
     {"Blue Positive Ring Rush", B_P_ringrush},
+    {"RED Negative Ring Rush", R_N_ringrush},
+    {"BLUE Negative Ring Rush", B_N_ringrush}
 });
 
 // tracking wheels
 pros::Rotation horizontalEnc(4);
-pros::Rotation verticalEnc(16);
-lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -3.75); 
-lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_275, 0.125); //0.125
+pros::Rotation verticalEnc(-18);
+lemlib::TrackingWheel horizontal(&horizontalEnc, lemlib::Omniwheel::NEW_275, -4); 
+lemlib::TrackingWheel vertical(&verticalEnc, lemlib::Omniwheel::NEW_275, 0.0625); //0.125
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&leftMotors, // left motor group
                               &rightMotors, // right motor group
-                              10.75, // 10 inch track width
+                              10.25, // 10 inch track width
                               lemlib::Omniwheel::NEW_325, // using new 4" omnis
                               450, // drivetrain rpm is 360
                               2 // horizontal drift is 2. If we had traction wheels, it would have been 8
 );
 
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(9, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              55, // derivative gain (kD)
+                                              30, // derivative gain (kD)
                                               0, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              300, // large error range timeout, in milliseconds
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
 // angular PID controller
 lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              13, // derivative gain (kD)
+                                              11, // derivative gain (kD)
                                               0, // anti windup
                                               0, // small error range, in inches
                                               0, // small error range timeout, in milliseconds
@@ -179,7 +238,7 @@ void initialize() {
         while (true) {
             // print robot location to the brain screen
             // brain.printf("X: %f", chassis.getPose().x); // x
-            // brain.printf("\nY: %f", chassis.getPose().y); // y
+            brain.printf("\nY: %f", chassis.getPose().y); // y
             // brain.printf("\nTheta: %f", chassis.getPose().theta); // heading
             // log position telemetry
             lemlib::telemetrySink()->info("Chassis pose: {}", chassis.getPose());
@@ -200,7 +259,6 @@ void initialize() {
 
     // Color Sort Task
     pros::Task colorSortTaskHandler(colorSortTask);
-    
 }
 
 void disabled() {}
@@ -208,15 +266,17 @@ void competition_initialize() {}
 
 void autonomous() {
 
-    isAutonMode = true;
+    // isAutonMode = true;
     sorterEnabled = true;
-    hang.extend();
+    extendMogo();
+    oSensor.set_led_pwm(100);
 
     selector.run_auton();
 }
 
 void opcontrol() {
 
+    lbTaskEnabled = true;
     bool rDoinkerToggle = false;
     bool lDoinkerToggle = false;
 
@@ -227,9 +287,8 @@ void opcontrol() {
         // move the chassis with curvature drive
         chassis.arcade(leftY, rightX);
 
-        isAutonMode = false;
+        // isAutonMode = false;
         sorterEnabled = false;
-        hang.retract();
 
 		// Intake controls
         if (controller.get_digital(DIGITAL_R1)){
@@ -282,6 +341,10 @@ void opcontrol() {
                 doinkerL.retract();
                 lDoinkerToggle = false;
             }
+        }
+
+        if(controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT)){
+            rSensor.reset_position();
         }
 
         controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A);
